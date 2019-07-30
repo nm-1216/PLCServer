da57d0dd2e294f238fd8334568b2dd7238ab77b2
黄望喜 <o7o@outlook.com>
2019-07-30 17:28:31 +0800

    添加日志文件

--- PLCServer/Core/ServerFactory.cs
+++ PLCServer/Core/ServerFactory.cs
@@ -29,7 +29,7 @@ namespace PLCServer
 
         internal static string DebugFile
         {
-            get { return $"{AppDomain.CurrentDomain.BaseDirectory}/{DateTime.Now.ToString("yyyyMMdd")}.txt"; }
+            get { return $"{AppDomain.CurrentDomain.BaseDirectory}/{{0}}"; }
         }
 
         // ReSharper disable once InconsistentNaming
@@ -82,7 +82,14 @@ namespace PLCServer
 
             if (null != tmp)
             {
-                tmp.LogNet = new HslCommunication.LogNet.LogNetSingle(DebugFile);
+                tmp.LogNet = new HslCommunication.LogNet.LogNetDateTime(string.Format(DebugFile, config.Name));
+                
+//logNet.SetMessageDegree(HslMessageDegree.DEBUG);//所有等级存储
+//logNet.SetMessageDegree(HslMessageDegree.INFO);//除DEBUG外，都存储
+//logNet.SetMessageDegree(HslMessageDegree.WARN);//除DEBUG和INFO外，都存储
+//logNet.SetMessageDegree(HslMessageDegree.ERROR);//只存储ERROR和FATAL
+//logNet.SetMessageDegree(HslMessageDegree.FATAL);//只存储FATAL
+//logNet.SetMessageDegree(HslMessageDegree.None);//不存储任何等级
 
                 tmp.SetPersistentConnection();
                 var rst = tmp.ConnectServer();
